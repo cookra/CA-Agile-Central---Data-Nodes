d4b21c85de690d2793536cc37a0360541634b4d1
Richard Cook <g01066236@barclays.com>
2017-03-27 23:23:08 +0100

    Sigma

--- App.js
+++ App.js
@@ -2,10 +2,58 @@ Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
     launch: function () {
-        //Write app code here
-        this.add(Ext.create('Ext.container.Container'),{style:'background:black',html:'<div id="cy"></div>'});
-        var cy = cytoscape({
-            container: document.getElementById('cy') // container to render in
+
+
+        var i,
+            s,
+            N = 100,
+            E = 500,
+            g = {
+                nodes: [],
+                edges: []
+            };
+
+        // Generate a random graph:
+        for (i = 0; i < N; i++)
+            g.nodes.push({
+                id: 'n' + i,
+                label: 'Node ' + i,
+                x: Math.random(),
+                y: Math.random(),
+                size: Math.random(),
+                color: '#666'
+            });
+
+        for (i = 0; i < E; i++)
+            g.edges.push({
+                id: 'e' + i,
+                source: 'n' + (Math.random() * N | 0),
+                target: 'n' + (Math.random() * N | 0),
+                size: Math.random(),
+                color: '#ccc'
+            });
+        // sigma.renderers.def = sigma.renderers.canvas
+        // Instantiate sigma:
+        s = new sigma({
+            graph: g,
+            container: 'graph-container'
         });
+
+        // Initialize the dragNodes plugin:
+        var dragListener = sigma.plugins.dragNodes(s, s.renderers[0]);
+
+        dragListener.bind('startdrag', function (event) {
+            console.log(event);
+        });
+        dragListener.bind('drag', function (event) {
+            console.log(event);
+        });
+        dragListener.bind('drop', function (event) {
+            console.log(event);
+        });
+        dragListener.bind('dragend', function (event) {
+            console.log(event);
+        });
+
     }
 });
